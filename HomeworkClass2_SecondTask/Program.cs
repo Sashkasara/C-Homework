687edce1d74661427d6a42dd9ac377da0917c8cd
Sashkasara <sarafinovska.sashka@yahoo.com>
2021-03-28 22:48:25 +0200

    HomeworkClass2

---
+++ HomeworkClass2_SecondTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeworkClass2_SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write first number: ");
+            bool firstInput = int.TryParse(Console.ReadLine(), out int firstParsedNumber);
+
+            Console.WriteLine("Please write second number");
+            bool secondInput = int.TryParse(Console.ReadLine(), out int secondParsedNumber);
+
+            Console.WriteLine("Please write third number");
+            bool thirdInput = int.TryParse(Console.ReadLine(), out int thirdParsedNumber);
+
+            Console.WriteLine("Please write fourth number");
+            bool fourthInput = int.TryParse(Console.ReadLine(), out int fourthParsedNumber);
+
+           if (firstInput && secondInput && thirdInput && fourthInput)
+            {
+                int result = (firstParsedNumber + secondParsedNumber + thirdParsedNumber + fourthParsedNumber) / 4;
+                Console.WriteLine("The average of the four numbers is " + result);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
