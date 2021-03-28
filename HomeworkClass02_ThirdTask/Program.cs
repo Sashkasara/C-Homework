97b1f23e492488ca1f3de79a9d87c23cf6828dfd
Sashkasara <sarafinovska.sashka@yahoo.com>
2021-03-28 23:03:34 +0200

    HomeworkSecondClass

---
+++ HomeworkClass02_ThirdTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeworkClass02_ThirdTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Swaping exercise
+            Console.WriteLine("Enter the first number");
+            int firstInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            int secondInput = int.Parse(Console.ReadLine());
+
+            int swap = firstInput;
+            firstInput = secondInput;
+            secondInput = swap;
+
+            Console.WriteLine("After swaping ");
+            Console.WriteLine("First number is: " + firstInput);
+            Console.WriteLine("Second Number is " + secondInput);
+
+            Console.ReadLine();
+        }
+    }
+}
