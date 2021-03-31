fd633aa4487e0b1eadcd8ef5298fa4409fdf2a45
Sashkasara <sarafinovska.sashka@yahoo.com>
2021-03-31 22:49:56 +0200

    SumOfEven and StudentGroup Exercise

---
+++ Class_03 - Loops and Arrays - Homework/Class_03_Homework_StudentGroupExercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Class_03_Homework_StudentGroupExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Make a new console application called StudentGroup
+            //Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+            //Get a number from the console between 1 and 2 and print the students from that group in the console.
+            //Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+            //Test Data:
+            //Enter student group: (there are 1 and 2 )
+            //1
+            //Expected Output:
+            //The Students in G1 are:
+            //Zdravko
+            //Petko
+            //Stanko
+            //Branko
+            //Trajko
+
+            string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = new string[] { "Sashka", "Nikola", "Dame", "Emilija", "Stefan" };
+
+            Console.WriteLine("Enter student group");
+            string groupInput = Console.ReadLine();
+
+            if (groupInput == "1")
+            {
+                Console.WriteLine("The students of group 1 are: ");
+                foreach (string student in studentsG1)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else if (groupInput == "2")
+            {
+                Console.WriteLine("The students of group 2 are: ");
+                foreach (string student in studentsG2)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid value");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class_03 - Loops and Arrays - Homework/Class_03_Homework_SumOfEven/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Class_03_Homework_SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     //Make a console application called SumOfEven. Inside it create an array of 6 integers.
+     //Get numbers from the input, find and print the sum of the even numbers from the array:
+
+            //Test Data:
+             //Enter integer no.1:
+            //4
+            //Enter integer no.1:
+            //3
+            //Enter integer no.1:
+            //7
+            //Enter integer no.1:
+            //3
+            //Enter integer no.1:
+            //2
+            //Enter integer no.1:
+            //8
+            //Expected Output:
+            //The result is: 14
+
+
+            int[] arrayOfNumbers = new int[6];
+            int sum = 0;
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                Console.Write("Enter a number: ");
+                string numberInput = Console.ReadLine();
+                bool conversionToNumber = int.TryParse(numberInput, out int num);
+
+                if (!conversionToNumber)
+                {
+                    i--;
+                    continue;
+                }
+
+                arrayOfNumbers[i] = num;
+            }
+
+            foreach (int number in arrayOfNumbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine($"The result is: {sum}.");
+            Console.ReadLine();
+        }
+    }
+}
