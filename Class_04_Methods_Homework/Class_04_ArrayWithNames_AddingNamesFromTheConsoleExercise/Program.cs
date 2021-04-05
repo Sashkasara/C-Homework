46aca9af996ab81585b2705e4fb63e73e1ceb409
Sashkasara <sarafinovska.sashka@yahoo.com>
2021-04-05 22:17:01 +0200

    Exercises

---
+++ Class_04_Methods_Homework/Class_04_ArrayWithNames_AddingNamesFromTheConsoleExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Class_04_ArrayWithNames_AddingNamesFromTheConsoleExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an array with names
+            //●Give an option to the user to enter a name from the keyboard
+            //●After entering the name put it in the array●Ask the user if they want to enter another name(Y / N)
+            //●Do the same process over and over until the user enters N
+            //●Print all the names after user enters N
+
+            string[] names = new string[2];
+            names[0] = "Damjan";
+            names[1] = "Trajan";
+            string newName;
+            string userInput;
+            do
+            {
+                Console.WriteLine("Please enter a name");
+                newName = Console.ReadLine();
+                Array.Resize(ref names, names.Length + 1);
+                names[names.Length - 1] = newName;
+                Console.WriteLine("You have entered the name " + newName + " in the array");
+                Console.WriteLine("Would you like to enter an other name?");
+                userInput = Console.ReadLine();
+                if (userInput != "y" && userInput != "Y")
+                {
+                    Console.WriteLine("The names in the array are ");
+                    foreach (var name in names)
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            } while (userInput == "y" || userInput == "Y");
+            Console.Read();
+        }
+    }
+}
---
+++ Class_04_Methods_Homework/Class_04_SumTheValuesFromTheArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class_04_SumTheValuesFromTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a new array of integers with 5 elements
+            //●Initialize the array elements  with values from input
+            //●Sum all the values and print the result in the console
+            int[] numbersArray = new int[5];
+            int sum = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Please enter a number");
+                string numberInput = Console.ReadLine();
+                bool isValidNumber = int.TryParse(numberInput, out int number);
+                if (isValidNumber)
+                {
+                    numbersArray[i - 1] = number;
+                    Console.WriteLine($"The {i} number that was added in the array is: {number}");
+                    sum += number;
+                    continue;
+                }
+                Console.WriteLine("The added input was not valid number. The program will count it as 0.");
+            }
+            Console.WriteLine($"The sum of the added numbers is {sum}");
+            Console.ReadLine();
+        }
+    }
+}
