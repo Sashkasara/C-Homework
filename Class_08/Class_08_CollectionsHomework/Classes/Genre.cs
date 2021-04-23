efd7b9c3fff6aa5dfb30260a35fd7d3c779b1cb1
Sashkasara <sarafinovska.sashka@yahoo.com>
2021-04-23 21:23:26 +0200

    Homework_Class_08_Collections

---
+++ Class_08/Class_08_CollectionsHomework/Classes/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_08_CollectionsHomework.Classes
+{
+    public enum Genre
+    {
+        Rock,
+        Hip_Hop,
+        Techno,
+        Classical
+    }
+}
---
+++ Class_08/Class_08_CollectionsHomework/Classes/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_08_CollectionsHomework.Classes
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Genre FavoriteMusicType { get; set; }
+        public List<Song> FavoriteSongs { get; set; }
+
+        public Person(string firstName, string lastName, int age, Genre favoritMusic)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            FavoriteMusicType = favoritMusic;
+            FavoriteSongs = new List<Song>();
+        }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
